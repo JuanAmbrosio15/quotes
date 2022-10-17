aca1cd4de90cc7f368185f1d8e18fe099d27538c
JuanAmbrosio15 <juano150798@gmail.com>
2022-10-17 12:37:20 -0500

    first commit

---
+++ app/src/main/java/com/example/quotes/domain/usercase/GetQuoteUseCase.kt
@@ -0,0 +1,4 @@
+package com.example.quotes.domain.usercase
+
+class GetQuoteUseCase {
+}
---
+++ app/src/main/java/com/example/quotes/presentation/view/QuoteRandomActivity.kt
@@ -0,0 +1,45 @@
+package com.example.quotes.presentation.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.quotes.databinding.ActivityQuoteRandomBinding
+import com.example.quotes.presentation.viewmodel.QuoteViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class QuoteRandomActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQuoteRandomBinding
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityQuoteRandomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //----------------------------
+        quoteViewModel.randomQuote()
+        observer()
+        binding.viewContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+        }
+
+    }
+    private fun observer(){
+        lifecycleScope.launch {
+            quoteViewModel.quoteModel.collect {
+                binding.tvQuote.text = it.quote
+                binding.tvAuthor.text= it.author
+            }
+        }
+    }
+
+
+}
+
+
